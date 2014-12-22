7c88e960a3f3d456cb61d2ed7dc6881a5f7c0f3b
Nighty22Night <lhote.antoine@gmail.com>
2014-12-22 16:44:39 +0100

    Add game class

---
+++ include/Game.h
@@ -0,0 +1,16 @@
+#ifndef GAME_H
+#define GAME_H
+
+
+class Game
+{
+    public:
+        Game();
+        ~Game();
+        void Idle(double delta_time);
+    protected:
+    private:
+
+};
+
+#endif // GAME_H
