07969ce96503a4ce4f9ad895c5757e06fadce754
Nighty22Night <lhote.antoine@gmail.com>
2014-12-23 11:22:47 +0100

    Affichage du score

--- src/Game.cpp
+++ src/Game.cpp
@@ -13,8 +13,9 @@ Game::~Game()
 
 void Game::Draw()
 {
-
-vBitmapOutput(-1.0,0.5f,"Score : ",GLUT_BITMAP_HELVETICA_18, 1, 1, 1);
+char *txt_score = new char;
+sprintf(txt_score, "Score : %d", this->m_score);
+vBitmapOutput(-1.0,0.5f,txt_score,GLUT_BITMAP_HELVETICA_18, 1, 1, 1);
 
 }
 
